4168279c23806e561fc1e261609460dec8ea653f
thaihunghung <110120031@st.tvu.edu.vn>
2024-05-04 12:08:37 +0700

    hhhh

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize,QueryTypes  } = require('sequelize');
 
 
 // kết nối docker
@@ -7,17 +7,20 @@ const { Sequelize } = require('sequelize');
 // });
 
 // kết nối localhost
-const sequelize = new Sequelize('tvu', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize('TVU', 'AdminTVU', 'CongNgheThongTin-DA20TTB', {
+  host: '103.200.20.110',
   dialect: 'mysql',
 });
-
 
 // Hàm kiểm tra kết nối
 async function testConnection() {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    
+    // Thực hiện một truy vấn SQL đơn giản
+    const result = await sequelize.query('SELECT * FROM `plos` WHERE 1', { type: QueryTypes.SELECT });
+    console.log('Query result:', result);
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
